9ee8fb1e31d6c1bb228ce59cfa6c879ee7349212
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-02-09 14:21:54 +0530

    Create 2.print in word.c

---
+++ 2.print in word.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 0: 
+printf("ZERO \n");
+break;
+case 1:
+printf("ONE \n"); 
+break;
+case 2: 
+printf("TWO \n");
+break;
+case 3:
+printf("THREE \n");
+break;
+case 4:
+printf("FOUR \n");
+break;
+case 5:
+printf("FIVE \n");
+break;
+case 6:
+printf("SIX \n");
+break;
+case 7:
+printf("SEVEN \n");
+break;
+case 8:
+printf("EIGHT \n");
+break;
+case 9:
+printf("NINE \n");
+break;
+case 10:
+printf(" TEN \n");
+break;
+return 0;
+}
+}
