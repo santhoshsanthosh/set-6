72ba41b9748908e9f2e523403578ddb2d254081a
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-02-20 11:28:12 +0000

    Create 8.swap.c

---
+++ 8.swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int i=30;
+	int i=40;
+	printf("i=%d before swapping\n",i,k);
+	i=i^k;
+	k=i^k;
+	i=i^k;
+	printf("i=%d k=%d after swapping\n",i,k);
+	return 0;
+}
