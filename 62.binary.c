03862e4f1633aa63414408ba7e910f57c914f584
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-02-27 10:58:59 +0000

    Update 62.binary.c

--- 62.binary.c
+++ 62.binary.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+int main() 
+{
 	int i,count=0,length=0;
 	char s[100];
-            scanf("%s",&s);
+         scanf("%s",&s);
 	for(i=0;s[i]!='\0';i++)
-	{           length++;
+	{           
+		length++;
 		if(s[i]=='0'||s[i]=='1')
 		count++;
 	}
