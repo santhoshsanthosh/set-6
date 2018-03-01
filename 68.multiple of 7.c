c9aa72d6d413872947977d44c52ea2bca38a03bb
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-03-01 11:11:11 +0000

    Create 68.multiple of 7.c

---
+++ 68.multiple of 7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	scanf("%d",&a);
+	printf("enter the number:%d",a);
+	if(a%7==0)
+	{
+		printf("\nyes");
+	}
+	else
+	{
+		printf("\nN0");
+	}
+	
+	return 0;
+}
