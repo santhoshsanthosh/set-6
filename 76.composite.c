cfd2d3847a2629654964b64c1760d5e70a8512ee
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-03-13 11:15:03 +0000

    Create 76.composite.c

---
+++ 76.composite.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b;
+	scanf("%d",&a);
+    if((a%2==0)||(a%3==0))
+    {
+    	printf("Yes");
+    }
+    else
+    {
+    	printf("No");
+    }
+	
+	return 0;
+}
