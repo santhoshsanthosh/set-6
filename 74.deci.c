ff6012c1790de30069dd6034fd0331d502fe3e22
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-03-13 10:34:16 +0000

    Create 74.deci.c

---
+++ 74.deci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int b,c;
+	float a;
+scanf("%f",&a);
+b=a*10;
+c=(b/10)+1;
+printf("the input number is:%.1f",a);
+printf("\noutput number is:%d",c);
+	return 0;
+}
