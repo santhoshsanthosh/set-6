b6f3d621d081a10fe6202bfc0063c3e5ac0248a5
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-03-16 11:27:21 +0000

    Create 80.print odd num.c

---
+++ 80.print odd num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the number");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+	b=a%10;
+	if((b%2)!=0)
+	printf(" %d",b);
+	a=a/10;
+	}
+	return 0;
+	
+}
